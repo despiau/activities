1f04c07ff46f69cf7dda506ef99a1f5e4445554c
Luis Despiau <30500498+despiau@users.noreply.github.com>
2023-11-30 15:12:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,65 @@
 document.addEventListener('DOMContentLoaded', function () {
     const categorySelector = document.getElementById('categorySelector');
     const imageGallery = document.getElementById('imageGallery');
+    const fixedBar = document.querySelector('.fixed-bar');
 
-    // Datos de ejemplo para cada categoría
+    // Images for each category
     const imagesData = {
         '1': [
-            { src: 'images/1-1.jpeg', label: 'Drawing' },
-            { src: 'images/1-2.jpeg', label: 'Reading' },
-            { src: 'images/1-3.jpeg', label: 'Playing' },
-            { src: 'images/1-4.jpeg', label: 'Singing' },
-            { src: 'images/1-5.jpeg', label: 'Dancing' },
-            { src: 'images/1-6.jpeg', label: 'Running' },
-            { src: 'images/1-7.jpeg', label: 'Jumping' },
-            { src: 'images/1-8.jpeg', label: 'Swimming' },
-            { src: 'images/1-9.jpeg', label: 'Painting' }
+            { src: 'images/1-1.jpg', label: 'Drawing' },
+            { src: 'images/1-2.jpg', label: 'Reading' },
+            { src: 'images/1-3.jpg', label: 'Playing' },
+            { src: 'images/1-4.jpg', label: 'Singing' },
+            { src: 'images/1-5.jpg', label: 'Dancing' },
+            { src: 'images/1-6.jpg', label: 'Running' },
+            { src: 'images/1-7.jpg', label: 'Jumping' },
+            { src: 'images/1-8.jpg', label: 'Swimming' },
+            { src: 'images/1-9.jpg', label: 'Painting' }
         ],
         '2': [
-            { src: 'images/2-1.jpeg', label: 'Apple' },
-            { src: 'images/2-2.jpeg', label: 'Fries' },
-            { src: 'images/2-3.jpeg', label: 'Pizza' },
-            { src: 'images/2-4.jpeg', label: 'Leaves' },
-            { src: 'images/2-5.jpeg', label: 'Candy' },
-            { src: 'images/2-6.jpeg', label: 'Chicken Nuggets' },
-            { src: 'images/2-7.jpeg', label: 'Apple Juice' },
-            { src: 'images/2-8.jpeg', label: 'Water' },
-            { src: 'images/2-9.jpeg', label: 'Watermelon' }
+            { src: 'images/2-1.jpg', label: 'Apple' },
+            { src: 'images/2-2.jpg', label: 'Fries' },
+            { src: 'images/2-3.jpg', label: 'Pizza' },
+            { src: 'images/2-4.jpg', label: 'Leaves' },
+            { src: 'images/2-5.jpg', label: 'Candy' },
+            { src: 'images/2-6.jpg', label: 'Chicken Nuggets' },
+            { src: 'images/2-7.jpg', label: 'Apple Juice' },
+            { src: 'images/2-8.jpg', label: 'Water' },
+            { src: 'images/2-9.jpg', label: 'Watermelon' }
         ],
         '3': [
-            { src: 'images/3-1.jpeg', label: 'Happy' },
-            { src: 'images/3-2.jpeg', label: 'Sad' },
-            { src: 'images/3-3.jpeg', label: 'Angry' },
-            { src: 'images/3-4.jpeg', label: 'Surprised' },
-            { src: 'images/3-5.jpeg', label: 'Scared' },
-            { src: 'images/3-6.jpeg', label: 'Excited' },
-            { src: 'images/3-7.jpeg', label: 'Tired' },
-            { src: 'images/3-8.jpeg', label: 'Bored' },
-            { src: 'images/3-9.jpeg', label: 'Curious' }
+            { src: 'images/3-1.jpg', label: 'Happy' },
+            { src: 'images/3-2.jpg', label: 'Sad' },
+            { src: 'images/3-3.jpg', label: 'Angry' },
+            { src: 'images/3-4.jpg', label: 'Surprised' },
+            { src: 'images/3-5.jpg', label: 'Scared' },
+            { src: 'images/3-6.jpg', label: 'Excited' },
+            { src: 'images/3-7.jpg', label: 'Tired' },
+            { src: 'images/3-8.jpg', label: 'Bored' },
+            { src: 'images/3-9.jpg', label: 'Curious' }
         ]
     };
 
-    // Función para cargar imágenes
+    // Function to load images based on the selected category
     function loadImages(category) {
-        imageGallery.innerHTML = ''; // Limpia la galería
+        imageGallery.innerHTML = ''; // Clear gallery
 
-        // Carga las imágenes para la categoría seleccionada
         imagesData[category].forEach(item => {
             const div = document.createElement('div');
             div.className = 'gallery-item';
             div.innerHTML = `<img src="${item.src}" alt="${item.label}"><p>${item.label}</p>`;
+            div.addEventListener('click', function() {
+                fixedBar.textContent = `Selected: ${item.label}`;
+            });
             imageGallery.appendChild(div);
         });
     }
 
-    // Evento para manejar cambios en el selector de categoría
+    // Event listener for category selection
     categorySelector.addEventListener('change', function () {
         loadImages(this.value);
     });
 
-    // Carga inicial de imágenes
+    // Initial load of images
     loadImages(categorySelector.value);
 });
